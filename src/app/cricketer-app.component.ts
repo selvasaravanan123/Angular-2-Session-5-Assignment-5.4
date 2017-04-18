f07709284bb45c04c36f0992954720b317bd24a0
selvasaravanan123 <selvasaravanan123@gmail.com>
2017-04-18 23:35:13 +0530

    Assignment 5.4

---
+++ src/app/cricketer-app.component.ts
@@ -0,0 +1,12 @@
+import { Component, ViewEncapsulation, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './cricketer-app.component.html',
+  styleUrls: ['./cricketer-app.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
+})
+
+export class AppComponent implements OnInit {
+  ngOnInit() {}
+}
